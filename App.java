72f61a69744020d9e0aca4a15a349dec0ead46ab
Blake <50782183+blakeseekings@users.noreply.github.com>
2021-02-13 03:11:27 -0500

    Update Files

--- App.java
+++ App.java
@@ -1,3 +1,4 @@
+
 /**
  * Allows users to interact with the criminal database software.
  * @author Blake Seekings
@@ -6,28 +7,30 @@
 public class App {
   private UserManager userMan;
   
-  
+/*-------------------------------------------------------------------------------------------------
+######################################### Object Creation #########################################
+--------------------------------------------------------------------------------------------------*/
 
   public App() {
     UserManager.createManager(this);
   }
 
-
-/*
----------------------------------------------------------------------------------------------------
+/*-------------------------------------------------------------------------------------------------
 #################################### Managing User Interaction ####################################
----------------------------------------------------------------------------------------------------
-*/
+---------------------------------------------------------------------------------------------------*/
 
   public void setManager(UserManager manager) {
     this.userMan = manager;
-  
   }
+
   public void login(String username, String password) {
     if (username == null || password == null)
       return;
     this.userMan.login(username,password);
   }
 
-  public void logout(){this.userMan.logout();}
+  public void logout() {
+    this.userMan.logout();
+  }
+
 }
---
+++ FrontEnd.java
@@ -0,0 +1,13 @@
+
+/**
+ * Possible Additions:
+ *  - GUI Class, handles the printing of prompts and information.
+ *  - UI CLass, handles user input. |--> GUI.
+ */
+public class FrontEnd {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+  
+}
